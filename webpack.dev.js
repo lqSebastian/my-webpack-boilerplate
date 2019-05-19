69da267492504026c9be18d0c00c1566abd2d64d
Sebastian <sebastian.luque.pl@gmail.com>
2019-05-18 22:51:36 -0500

    first commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,35 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
+    devtool: "eval-cheap-module-source-map",
+    entry: {
+        index: "./src/index.js"
+    },
     devServer: {
         port: 8080,
         contentBase: path.join(__dirname, "dist")
     },
     node: {
-        fs: 'empty'
+        fs: "empty"
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 options: {
-                    presets: ['env']
+                    presets: ["env"]
                 }
             },
             {
                 test: /\.(scss|css)$/,
                 use: [
+                    // MiniCss in place of style-loader since it had an ugly delay on loading css
                     {
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
+                        loader: MiniCssExtractPlugin.loader
                     },
                     {
                         // translates CSS into CommonJS
@@ -40,48 +39,62 @@ module.exports = {
                         }
                     },
                     {
+                        // Runs compiled CSS through postcss for vendor prefixing
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: false
+                        }
+                    },
+                    {
                         // compiles Sass to CSS
                         loader: "sass-loader",
                         options: {
-                            outputStyle: 'expanded',
+                            outputStyle: "expanded",
                             sourceMap: true,
                             sourceMapContents: true
                         }
                     }
-                    // Please note we are not running postcss here
                 ]
-            }
-            ,
+            },
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(ttf|eot|woff|woff2)$/,
                 use: [
                     {
-                        loader: 'url-loader',
+                        loader: "url-loader",
                         options: {
                             // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
+                            name: "fonts/[name].[ext]?hash=[hash:20]",
+                            limit: 7192
                         }
                     }
                 ]
-            }
-            ,
+            },
             {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: "file-loader",
+                options: {
+                    name: "[path][name].[ext]"
+                }
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: "html-loader",
+                    options: {
+                        interpolate: true
                     }
-                ]
+                }
             }
-        ],
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
+            template: "./src/pages/index.html",
             inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
         })
     ]
 };
